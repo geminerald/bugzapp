98a8220e9fbaca3322e55e44896afa0d4c4960df
geminerald <zacdwarner@gmail.com>
2020-06-20 12:56:07 +0000

    added about html page

--- home/views.py
+++ home/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 def index(request):
     """view to return home page"""
     return render(request, 'home/index.html')
+
+def about(request):
+    """view to render the about page"""
+    return render(request, 'home/about.html')
