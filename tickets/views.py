0e601f41c061351aa7e6b75fd863eadbd95b1838
geminerald <zacdwarner@gmail.com>
2020-06-20 19:25:32 +0000

    connected and loaded test tickets page

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def tickets(request):
+    """
+    Renders a page showing all currently open tickets
+    """
+    return render(request, 'tickets.html')
