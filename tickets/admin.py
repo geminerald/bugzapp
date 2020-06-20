cb5eacb475f8d635c8bd62608e54db5495c16cbd
geminerald <zacdwarner@gmail.com>
2020-06-20 19:30:57 +0000

    added tickets to admin section of site

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Ticket
 # Register your models here.
+
+
+class TicketAdmin(admin.ModelAdmin):
+    fields = ['title', 'type', 'status', 'score', 'description', ]
+    readonly_fields = ['user', 'description']
+
+
+admin.site.register(Ticket, TicketAdmin)
