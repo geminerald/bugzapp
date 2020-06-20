d2006de2f9b6d05bb82eaa8bfb4350ee670bea0c
geminerald <zacdwarner@gmail.com>
2020-06-20 19:47:49 +0000

    added view ticket view to views in tickets

--- tickets/views.py
+++ tickets/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Ticket
 
 # Create your views here.
 
@@ -7,4 +8,24 @@ def tickets(request):
     """
     Renders a page showing all currently open tickets
     """
-    return render(request, 'tickets.html')
+    tickets = Ticket.objects.all()
+
+    context = {
+        'tickets': tickets,
+    }
+
+    return render(request, 'tickets.html', context)
+
+
+def viewticket(request, ticket_id):
+    """
+    Renders a page showing one specific ticket
+    """
+
+    ticket = get_object_or_404(Ticket, pk=ticket_id)
+
+    context = {
+        'ticket': ticket,
+    }
+
+    return render(request, 'viewticket.html', context)
