45366bb034172ea924058effe4e918d1f75b5879
geminerald <zacdwarner@gmail.com>
2020-06-22 13:29:17 +0000

    added urls and basic html

--- bugzapp/settings.py
+++ bugzapp/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'tickets',
     'checkout',
     'products',
+    'cart',
 ]
 
 MIDDLEWARE = [
--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def cart(request):
 
+def cart(request):
+    """
+    Shows a user the cart page
+    """
 
     return render(request, 'cart/cart.html')
