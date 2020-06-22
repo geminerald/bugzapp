28a57ee9a46914e917dbc109e11e3bcc7343fc82
geminerald <zacdwarner@gmail.com>
2020-06-22 15:08:31 +0000

    added add to cart view in cart app

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -9,3 +9,21 @@ def cart(request):
     """
 
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+
+    redirect_url = request.POST.get('redirect_url')
+
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+
+    return redirect(redirect_url)
