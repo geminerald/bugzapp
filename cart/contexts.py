abc0850cd6786ff460e892acdc3189ebf8a7c2ed
geminerald <zacdwarner@gmail.com>
2020-06-22 14:03:14 +0000

    added variables to context

--- cart/contexts.py
+++ cart/contexts.py
@@ -2,6 +2,14 @@
 
 def cart_contents(request):
 
-    context = {}
+    cart_items = []
+    total = 0
+    product_count = 0
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count
+    }
 
     return context
