edb6c11b87e2b4a981f4e3a85cc528046537ea85
geminerald <zacdwarner@gmail.com>
2020-06-22 11:58:39 +0000

    added internal functions to checkout models

--- bugzapp/settings.py
+++ bugzapp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'home',
     'tickets',
     'checkout',
+    'products',
 ]
 
 MIDDLEWARE = [
--- checkout/models.py
+++ checkout/models.py
@@ -20,6 +20,14 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _generate_order_number(self):
+        return uuid.uuid4().hex,upper()
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
 
 class OrderLine(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
