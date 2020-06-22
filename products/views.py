d4fb9d3e685ebe55383d8d864e2e0636d4a3938b
geminerald <zacdwarner@gmail.com>
2020-06-22 13:20:52 +0000

    added products html page and updated views, urls and admin to ensure running

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
 
 # Create your views here.
+
+
+def products(request):
+    """
+    Renders a page showing all currently open tickets
+    """
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products.html', context)
