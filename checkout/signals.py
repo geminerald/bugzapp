6cc048c359ae0ae6f20308e842bfcef56f79035f
geminerald <zacdwarner@gmail.com>
2020-06-29 09:19:02 +0000

    added forms and signals to checkout app

---
+++ checkout/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import OrderLine
+
+
+@receiver(post_save, sender=OrderLine)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    updates and order total on a line item in the order
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLine)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    updates and order total on a line item delete
+    """
+    instance.order.update_total()
