54878881d31d883921115685ac3ae6c5a1175d7e
geminerald <zacdwarner@gmail.com>
2020-06-29 13:25:12 +0000

    updated route to checkout page and cart page ordering to allow for transition between cart and checkout

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,7 @@ def checkout(request):
         return redirect(reverse('products'))
 
     order_form = OrderForm()
-    template = 'checkout/checkout.html'
+    template = 'checkout.html'
     context = {
         'order_form': order_form
     }
