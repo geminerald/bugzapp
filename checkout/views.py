1a1a4709abfdeb0ee617fe80bb1cbe8484e71ab9
geminerald <zacdwarner@gmail.com>
2020-07-02 18:08:22 +0000

    added stripe keys to views page and intent

--- checkout/views.py
+++ checkout/views.py
@@ -10,6 +10,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There is nothing in your cart")
@@ -18,13 +21,22 @@ def checkout(request):
     current_cart = cart_contents(request)
     total = current_cart['total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing, did you forget to add it?')
+
     template = 'checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_js0eHzBCLVxtiXt9XLKiZuAh00Cvn2euvv',
-        'client_secret': 'test_client_secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
