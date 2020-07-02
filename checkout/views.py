8af1c98db94efa7d7c2cc7d5ad569a6b7194a186
geminerald <zacdwarner@gmail.com>
2020-07-02 18:42:12 +0000

    added order add and save logic

--- checkout/views.py
+++ checkout/views.py
@@ -4,6 +4,9 @@ from .forms import OrderForm
 from cart.contexts import cart_contents
 from django.conf import settings
 
+from .models import OrderLine
+from products.models import Product
+
 import stripe
 
 # Create your views here.
@@ -13,30 +16,62 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "There is nothing in your cart")
-        return redirect(reverse('products'))
+    if request.methodv == 'POST':
+        cart = request.session.get('cart', {})
 
-    current_cart = cart_contents(request)
-    total = current_cart['total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number']
+        }
 
-    order_form = OrderForm()
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in cart.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    orderline = OrderLine(
+                        order=order,
+                        product=product,
+                        quantity=quantity
+                    )
+                    orderline.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        'You have selected an invalid product, please try again'
+                    ))
+                    order.delete()
+                    return redirect(reverse('view_cart'))
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error in your form')
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "There is nothing in your cart")
+            return redirect(reverse('products'))
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing, did you forget to add it?')
+        current_cart = cart_contents(request)
+        total = current_cart['total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
-    template = 'checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+        order_form = OrderForm()
 
-    return render(request, template, context)
+        if not stripe_public_key:
+            messages.warning(
+                request, 'Stripe public key is missing, did you forget to add it?')
+
+        template = 'checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+
+        return render(request, template, context)
