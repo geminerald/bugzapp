c16c8b35189cec5adcfd6360d9bf8fc3ec1d004e
geminerald <zacdwarner@gmail.com>
2020-07-17 10:16:44 +0000

    set up whitenoise for static files

--- bugzapp/settings.py
+++ bugzapp/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -182,12 +183,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media path
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+# For AWS Support. Currently using whitenoise - however leaving these
+# configurations here as may include facility to upload screenshots in
+# future which will use AWS
 """
 if 'USE_AWS' in os.environ:
 
