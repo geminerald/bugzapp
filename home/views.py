04fd511a4b8e54851a64d55badf6ed6e8ea27e30
geminerald <zacdwarner@gmail.com>
2020-07-24 14:25:28 +0000

    added test 404 page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 # Create your views here.
 
 
@@ -10,3 +11,11 @@ def index(request):
 def about(request):
     """view to render the about page"""
     return render(request, 'home/about.html')
+
+
+def error_404_view(request):
+    """
+    server error 404 handler
+    """
+    if request.DoesNotExist:
+        raise Http404
